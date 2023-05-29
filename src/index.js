880aed699fd696229448b76788768ef82584ca51
satellite-21 <www.kartiksuccess832@gmail.com>
2023-05-29 14:53:51 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+//now render 
+ReactDOM.render(
+<div>
+    <h1 className="heading_color" contentEditable='false' >Stay hungry, Stay Foolish </h1>
+    <div>
+        <img className="image-size" src="https://e0.pxfuel.com/wallpapers/217/384/desktop-wallpaper-hennie-funexplosie-08-steve-jobs-lr-stay-hungry-stay-foolish.jpg" alt="Steve_jobs1" ></img>
+        <img className="image-size" src="https://www.hollywoodreporter.com/wp-content/uploads/2015/03/steve_jobs_the_man_in_the_machine_still.jpg?w=3000" alt="Steve_jobs2"></img>
+        <img className="image-size" src="https://www.investopedia.com/thmb/SOrSZjDw7SXxt4ojtfF0soN-TY8=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/steve-jobs-apple-story_final-14a355d35609422798041dcfb2cb61f6.png" alt="Steve_jobs3"></img>
+    </div>
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+</div>, 
+
+document.getElementById("root")
+
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
